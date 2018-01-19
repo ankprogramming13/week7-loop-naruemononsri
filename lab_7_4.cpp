790a2f5079c54930b33fe87ef362bbb7e23a87e6
Unknown <noopang98@gmail.com>
2018-01-19 13:55:51 +0700

    ข้อ 4

--- lab_7_4.cpp
+++ lab_7_4.cpp
@@ -4,43 +4,31 @@ using namespace std;
 
 int main () {
 
+    // Local variable declaration:
+    int a = 0;
+
+    // while loop execution
+    while( a < 3 ) {
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+      a++;
+    }
+
   // Without loop
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
+
 
   // เขียนโปรแกรมให้ทำงานได้ตามข้างต้นโดยใช้ Loop
 
 
   return 0;
 }
+
